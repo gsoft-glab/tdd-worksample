f7af16389d463fdb7b309a4b6d156a04f991bbd8
Maxime Beaudoin <maxime.beaudoin@gsoft.com>
2022-11-01 09:45:41 -0400

    first commit

---
+++ TddWorkSample/AccountTests.cs
@@ -0,0 +1,11 @@
+namespace TddWorkSample
+{
+    public class AccountTests
+    {
+        [Test]
+        public void Test()
+        {
+            var account = new Account(...);
+        }
+    }
+}
